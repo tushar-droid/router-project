dfd55910c23411179cabec5c4f2372679b1f770b
tushar-droid <tusharsandhu3022@gmail.com>
2024-12-29 21:21:52 -0800

    Added Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { HStack } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
       <h1>React Router project.</h1>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   );
 }
